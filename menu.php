89a5fe01e2fea62d977c74d89bec4c0e18541d46
hnotroN <73133201+hnotroN@users.noreply.github.com>
2020-10-20 16:48:10 -0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <h1>Welcome!</h1>
+    <hr>
+    <style>
+    body {
+        background-color: #99DBD6;
+    }
+    </style>
+    <style type ="text/css" >
+        .footer{ 
+            position: fixed;     
+            text-align: center;    
+            bottom: 0px; 
+            width: 100%;
+        }  
+    </style>
+</head>
+<body>  
+    <h2>What would you like to play?</h2>
+    <h3>
+    <A HREF="temp_files/crazy_cars.php">Crazy Cars</A><br>
+    <A HREF="temp_files/whack_a_mole.php">Whack-A-Mole</A><br>
+    <A HREF="temp_files/change_it_up.php">Change It Up</A><br>
+    <A HREF="temp_files/block_builder.php">Block Builder</A><br>
+    <A HREF="temp_files/fine_line.php">Fine Line</A><br>
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <A HREF="lessons">Lessons</A><br>
+    <A HREF="login_page">Logout</A>
+    </h3>
+
+</body>
+
+    
