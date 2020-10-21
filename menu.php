4ad25be44d3a24a5f936190c63bafcc2516a1286
hnotroN <73133201+hnotroN@users.noreply.github.com>
2020-10-21 11:13:26 -0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -8,23 +8,15 @@
         background-color: #99DBD6;
     }
     </style>
-    <style type ="text/css" >
-        .footer{ 
-            position: fixed;     
-            text-align: center;    
-            bottom: 0px; 
-            width: 100%;
-        }  
-    </style>
 </head>
 <body>  
     <h2>What would you like to play?</h2>
     <h3>
-    <A HREF="temp_files/crazy_cars.php">Crazy Cars</A><br>
-    <A HREF="temp_files/whack_a_mole.php">Whack-A-Mole</A><br>
-    <A HREF="temp_files/change_it_up.php">Change It Up</A><br>
-    <A HREF="temp_files/block_builder.php">Block Builder</A><br>
-    <A HREF="temp_files/fine_line.php">Fine Line</A><br>
+    <A HREF="crazy_cars.php">Crazy Cars</A><br>
+    <A HREF="whack_a_mole.php">Whack-A-Mole</A><br>
+    <A HREF="change_it_up.php">Change It Up</A><br>
+    <A HREF="block_builder.php">Block Builder</A><br>
+    <A HREF="fine_line.php">Fine Line</A><br>
 
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <A HREF="lessons">Lessons</A><br>
