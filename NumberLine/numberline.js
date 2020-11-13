c07d439b3fd8e47efbbed9951c3b8108a4c00a07
lwgover <73131292+lwgover@users.noreply.github.com>
2020-11-12 22:26:45 -0600

    Update numberline.js

--- NumberLine/numberline.js
+++ NumberLine/numberline.js
@@ -20,26 +20,31 @@ scor.src = "sounds/score.mp3";
 
 */
 
-var targetNumber = 0;
 var currentNumber = 0;
-var maxNumber = 20;
+var maxNumber = 25;
+var minNumber = 0;
+var targetNumber = Math.round(minNumber + ((maxNumber - minNumber) * Math.random()));
+var treasureFound = false;
 
-var pirateX = 300; 
-var pirateY = 100;
+function myfunction() {
+	console.log("This is a working button");
+  }
 
-var CircleX = 100;
-var CircleY = 100;
 
 //detecting arrow key presses
 document.addEventListener('keydown', function(e) {
 	switch (e.keyCode) {
+		case 32:
+			//alert('left');
+	
+			break;
 		case 37:
 			//alert('left');
 			moveLeft();
-			console.log("LEFT");
 			break;
 		case 38:
 			moveUp();
+			
 		   //alert('up');
 			break;
 		case 39:
@@ -48,54 +53,91 @@ document.addEventListener('keydown', function(e) {
 			break;
 		case 40:
 			moveDown();
-			alert('down');
+			//alert('down');
+			break;
+		case 32:
+			treasureFound();
+			//alert('down');
 			break;
 	}
 });
 
 function moveLeft(){
-	CircleX -= 3;
+	currentNumber -= 0.1;
 	draw();
 }
 function moveRight(){
-	CircleX += 3;
+	currentNumber += 0.1;
 	draw();
 }
 function moveUp(){
-	CircleY -= 3;
 	draw();
 }
 function moveDown(){
-	CircleY += 3;
 	draw();
 }
 
 function draw(){
+	ctx.textAlign = 'center';
+	drawBackground();
+	drawMap();
 	
-
-		
-	ctx.fillStyle = "red";
-	ctx.fillRect(0, 0, 800, 400);
-	ctx.beginPath();
-	ctx.arc(CircleX, CircleY, 95, 0, 2 * Math.PI, false);
-	ctx.lineWidth = 10;
-	ctx.strokeStyle = '#000000';
-	ctx.stroke();
+	
+	
+	
+	
+	
+	
 	
 	//ctx.drawImage(bg,0,0); // draws background
 
 	//ctx.drawImage(fg,0,cvs.height - fg.height); // draws foreground
 	
 	//ctx.drawImage(pirate,bX,bY);
-	
-	ctx.fillStyle = "#000";
-	ctx.font = "20px Verdana";
-	ctx.fillText("Score : "+score,10,cvs.height-20);
-	
-	console.log('AHHHH');
-	
-	
-	requestAnimationFrame(draw);
+	drawIslands();
+	drawBoat();
+	//requestAnimationFrame(draw);
+	//win();
 }
-
+function drawBackground() {
+	ctx.fillStyle = '#7FAFFF';
+	ctx.fillRect(0, 0, 1400, 600);
+}
+function drawIslands(){
+	var i;
+	for(i = 0; i < 11; i++){
+		if((Math.round((currentNumber + 5 - i)) <= maxNumber) && (Math.round((currentNumber + 5 - i)) >= (minNumber))){
+			ctx.fillStyle = '#FFFF00';
+			ctx.fillRect((300 *  (Math.round(currentNumber) - currentNumber)) + (1770 - (300 * i)), 300,150,110);
+			ctx.fillStyle = '#000000';
+			ctx.fillText(Math.round(currentNumber + 5 - i), 75 + (300 * (Math.round(currentNumber) - currentNumber)) + (1770 - (300 * i)), 395);
+		}
+	}
+}
+function drawBoat(){
+	ctx.fillStyle = '#AA9481';
+	ctx.fillRect(320, 370, 115, 85);
+	ctx.fillStyle = '#000000';
+	ctx.font = "30px Arial";
+	ctx.fillText("Boat", 372, 424);
+}
+function drawMap(){
+	ctx.fillStyle = '#000000';
+	ctx.font = "30px Arial";
+	ctx.fillText(Math.round(targetNumber), 700, 50);
+	//ctx.fillText(currentNumber, 690, 50);
+}
+function win(){
+	ctx.fillStyle = '#CFAF3F';
+	ctx.fillRect(0, 0, 1400, 600);
+	ctx.fillStyle = '#000000';
+	ctx.fillText('You Found the Treasure!!!', 700, 300);
+}
+function reset(){
+	var currentNumber = 0;
+	var maxNumber = 25;
+	var minNumber = 0;
+	var targetNumber = Math.round(minNumber + ((maxNumber - minNumber) * Math.random()));
+	var treasureFound = false;
+}
 draw();
