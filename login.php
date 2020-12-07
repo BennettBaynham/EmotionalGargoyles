bf963e1267ede81fd8aa590d8859eccb88cc82f7
BennettBaynham <72760958+BennettBaynham@users.noreply.github.com>
2020-12-06 17:58:10 -0800

    Update login.php

--- login.php
+++ login.php
@@ -18,10 +18,20 @@
 
 
    <form action="my_functions.php" method="post">
-       username: <input type="text" name="u"><br><br>
-       password: <input type="password" name="pw">
+       Username: <input type="text" name="u"><br><br>
+       Password: <input type="password" name="pw">
        <input type="hidden" name="task" value="login">
        <button type="submit">Login</button>
+   </form><br><br><br><br><br><br>
+
+   <form action="my_functions.php" method="post">
+       Create Username: <input type="text" name="u"><br><br>
+       Create Password: <input type="password" name="pw"><br><br>
+       Student: <input type="radio" name='ut' value="1" checked>
+       Teacher:  <input type="radio" name='ut' value="2"><br><br>
+       Teacher Username (if student): <input type="text" name="t"><br><br>
+       <input type="hidden" name="task" value="register">
+       <button type="submit">Register</button>
    </form>
 </body>
 </html>
