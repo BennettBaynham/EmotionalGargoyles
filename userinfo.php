20b3a99673f7fce0afb335609f15ed68334846be
hollyjpotter <41168067+hollyjpotter@users.noreply.github.com>
2020-12-08 20:45:33 -0800

    Update userinfo.php

--- userinfo.php
+++ userinfo.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <link rel="stylesheet" href="creativeProfile.css">
 
 <center>
@@ -28,38 +30,47 @@
                           <ul>
                             <li><a href="menu_redirector.php">Home</a></li>
                             <li style="float:right"><a href="login.php">Log Out</a></li>
-                            <li style="float:right"><a class="active" href="userinfo.html">Profile</a></li>
+                            <li style="float:right"><a class="active" href="userinfo.php">Profile</a></li>
                             <li style="float:left"><a href="basicLessons.html">Basic Lessons</a></li>
                           </ul>
                         <div class="col-sm-8">
                             <div class="card-block">
-                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Games</h6>
                                 <div class="row">
                                     <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Email</p>
-                                        <h6 class="text-muted f-w-400">rntng@gmail.com</h6>
+                                        <p class="m-b-10 f-w-600">Crazy Cars</p>
+                                        <h6 class="text-muted f-w-400">Number of Correct Questions</h6>
+                                        <div class="box">
+                                            <p>1</p>
+                                        </div>
+                                        <h6 class="text-muted f-w-400">Number of Incorrect Questions</h6>
+                                        <div class="box">
+                                            <p>2</p>
+                                        </div>
                                     </div>
                                     <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Phone</p>
-                                        <h6 class="text-muted f-w-400">98979989898</h6>
+                                        <p class="m-b-10 f-w-600">Change it Up</p>
+                                        <h6 class="text-muted f-w-400">Number of Correct Questions</h6>
+                                        <div class="box">
+                                            <p>25</p>
+                                        </div>
+                                        <h6 class="text-muted f-w-400">Number of Incorrect Questions</h6>
+                                        <div class="box">
+                                            <p>600</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <p class="m-b-10 f-w-600">Fine Line</p>
+                                        <h6 class="text-muted f-w-400">Number of Correct Questions</h6>
+                                        <div class="box">
+                                            <p>7</p>
+                                        </div>
+                                        <h6 class="text-muted f-w-400">Number of Incorrect Questions</h6>
+                                        <div class="box">
+                                            <p>10</p>
+                                        </div>
                                     </div>
                                 </div>
-                                <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Recent</p>
-                                        <h6 class="text-muted f-w-400">Sam Disuja</h6>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <p class="m-b-10 f-w-600">Most Viewed</p>
-                                        <h6 class="text-muted f-w-400">Dinoter husainm</h6>
-                                    </div>
-                                </div>
-                                <ul class="social-link list-unstyled m-t-40 m-b-10">
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
-                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
-                                </ul>
                             </div>
                         </div>
                     </div>
